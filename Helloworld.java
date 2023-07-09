a3bf21412fb53b5afab76af7e55d37d5804f2a01
smahajan51 <108337389+smahajan51@users.noreply.github.com>
2023-07-09 11:09:37 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld
 {
-  public static void main(string args[])
+  public static void main(string.args[])
   {
     syntax.out.println("Hello World");
     syntax.out.printl(" Hi This program is for Devops POE");
