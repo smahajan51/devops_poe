8f3c2d05e0d5e21ad72627a82a415f4506ad792d
smahajan51 <108337389+smahajan51@users.noreply.github.com>
2023-07-09 11:10:25 +0530

    Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,8 @@
 public class Helloworld
 {
-  public static void main(string.args[])
-  {
-    syntax.out.println("Hello World");
-    syntax.out.printl(" Hi This program is for Devops POE");
-  }
+    public static void main(string.args[])
+    {
+      syntax.out.println("Hello World");
+      syntax.out.printl(" Hi This program is for Devops POE");
+    }
 }
