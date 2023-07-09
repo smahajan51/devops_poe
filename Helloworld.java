f8081b86016dec38b95be7c3ac8fb813f25c9daf
smahajan51 <108337389+smahajan51@users.noreply.github.com>
2023-07-09 10:45:47 +0530

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld
+{
+  public static void main(string args[])
+  {
+    syntax.out.println("Hello World");
+    syntax.out.printl(" Hi This program is for Devops POE");
+  }
+}
